14653f66730e049eb4e564882677431862e4a6fd
Lucas Lima <limalucks@hotmail.com>
2025-04-21 21:58:29 -0300

    ajustes nos endpoints de login e registro

--- backend/app/controllers/users/sessions_controller.rb
+++ backend/app/controllers/users/sessions_controller.rb
@@ -1,5 +1,19 @@
 class Users::SessionsController < Devise::SessionsController
     respond_to :json
+
+    # skip_before_action :verify_authenticity_token, only: [:create]
+    # skip_before_action :authenticate_user!, only: [:create]
+
+    def create
+      user = User.find_by(email: params[:email])
+  
+      if user&.valid_password?(params[:password])
+        token = Warden::JWTAuth::UserEncoder.new.call(user, :user, nil).first
+        render json: { token: token, user: user }, status: :ok
+      else
+        render json: { error: 'Invalid email or password' }, status: :unauthorized
+      end
+    end
   
     private
 
